2484b522fa2a51c07d7c76ad96ec0a0eb961e021
Dawa Ometto <dawa.ometto@phil.uu.nl>
2015-08-14 22:28:04 +0200

    Gollum::Editor.asset_paths yields asset paths. Fix syntax error in DEFAULT_OPTS.

--- lib/gollum-editor.rb
+++ lib/gollum-editor.rb
@@ -6,8 +6,22 @@ module Gollum
       defined?(ACTIVE_EDITOR) ? self.const_get("ACTIVE_EDITOR") : Gollum::Editor::Default
     end
 
+    def self.asset_paths
+    	assets = {'/css/' => [Gollum::Editor::Default.css_path], '/javascript' => [Gollum::Editor::Default.js_path]}
+    	active_editor_class = self.active_editor
+    	unless active_editor_class == Gollum::Editor::Default
+    		assets['/css/'] << active_editor_class.css_path
+    		assets['/javascript'] << active_editor_class.js_path
+    	end
+    	assets.each do |type, paths|
+    		paths.each do |path|
+    			yield type, path
+    		end
+    	end
+    end
+
     class Default
-      DEFAULT_OPTS = {:js_path => 'javascript', :css_path => = 'css', :editor_css => 'editor.css', :dialog_css => 'dialog.css', :highlight_css => 'highlightjs-github.css'}
+      DEFAULT_OPTS = {:js_path => 'javascript', :css_path => 'css', :editor_css => 'editor.css', :dialog_css => 'dialog.css', :highlight_css => 'highlightjs-github.css'}
 
       def self.path
         ::File.expand_path("..", __FILE__)
